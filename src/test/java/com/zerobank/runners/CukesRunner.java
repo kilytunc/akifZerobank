27e8d7ae7add4aafc06ab43759fb56faf2b4d7fc
MehmetAkifTUNC <kilyneu@gmail.com>
2020-10-27 15:29:16 +0100

    Cukesrunner

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -1,4 +1,24 @@
 package com.zerobank.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/deafult-html-reports"
+        }  ,
+        features = "src/test/resources/features"       ,
+        glue = "com/zerobank/stepdefinitions"          ,
+        dryRun = true  ,
+        tags = "@wip"
+)
+
+
 public class CukesRunner {
+
+
 }
